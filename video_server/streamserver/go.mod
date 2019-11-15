1209a7e0e7651f63f9fed5b904c00930a1c0dac4
1521350289@qq.com <1521350289@qq.com>
2019-11-16 01:10:20 +0800

    feat: add connectLimiter

---
+++ video_server/streamserver/go.mod
@@ -0,0 +1,5 @@
+module streamserver
+
+go 1.13
+
+require github.com/julienschmidt/httprouter v1.3.0
---
+++ video_server/streamserver/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func RegisterHandlers() *httprouter.Router {
+	router := httprouter.New()
+	router.GET("/videos/:vid-id", streamHandler)
+	router.POST("upload/:vid-id", uploadHandler)
+	return router
+}
+
+func main() {
+	r := RegisterHandlers()
+	http.ListenAndServe(":9000", r)
+}
